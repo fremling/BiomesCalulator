f9a8342f729ce784674640bb7fe5af9d1958ec52
Mikael Fremling <micke.fremling@gmail.com>
2022-01-13 15:38:35 +0100

    Some buttons and test funcitonnality.

--- app/src/main/java/com/example/biomescalculator/MainActivity.kt
+++ app/src/main/java/com/example/biomescalculator/MainActivity.kt
@@ -2,10 +2,79 @@ package com.example.biomescalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.biomescalculator.databinding.ActivityMainBinding
+
+//Varialbe for binding
+lateinit var binding: ActivityMainBinding
+
+val NoWeaponsList = mutableListOf(0,0,0)
+val WeaponNameList = listOf("Potato","Wood","Stone")
+val WeaponStrengthList = listOf(0,1,2)
+val NoWpnButtons = 3
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.BattleStats.text = "Welcome To the Battle Calculator\n\nPlease choose your weapons!"
+
+        //Initiate the list of weapons
+
+        UpdateWpnText()
+
+        binding.weapon1.setOnClickListener { AddWeapon(1) }
+        binding.weapon2.setOnClickListener { AddWeapon(2) }
+        binding.weapon3.setOnClickListener { AddWeapon(3) }
+
+        binding.Reset.setOnClickListener { ResetWeapons() }
+
+        binding.CalculateStats.setOnClickListener { CalulateStats() }
     }
+
+    private fun ResetWeapons() {
+        for (x in 0..(NoWpnButtons-1)) {NoWeaponsList[x] = 0}
+        binding.BattleStats.text = "Welcome To the Battle Calculator\n\nPlease choose your weapons!"
+        UpdateWpnText()
+    }
+
+    private fun AddWeapon(WpnNo: Int) {
+        NoWeaponsList[WpnNo - 1]++
+        UpdateWpnText()
+    }
+
+    private fun CalulateStats() {
+        //Sum the total number of wepons
+        val TotalNoWeapons =  NoWeaponsList.sum()
+        var TotalAttack = 0
+        for (x in 0..(NoWpnButtons-1)) {
+            TotalAttack += NoWeaponsList[x] * WeaponStrengthList[x]
+        }
+        val Str1 = "You have choosen ${TotalNoWeapons} weapons"
+        val Str2 = "The total strength is ${TotalAttack}"
+
+
+
+        binding.BattleStats.text = Str1 +"\n"+Str2
+    }
+
+
+    private fun UpdateWpnText() {
+        binding.NoWpn1.text = NoWeaponsList[0].toString()
+        binding.NoWpn2.text = NoWeaponsList[1].toString()
+        binding.NoWpn3.text = NoWeaponsList[2].toString()
+    }
+}
+
+fun <T> generate(size: Int, value: T): MutableList<T> {
+    return (0 until size).map { value }.toMutableList()
+}
+fun getProbList(WpnList : List<Int>,WpnPowList: List<Int>,NumCards : Int): List<Double> {
+
+    val ReturnList : MutableList<Double> = generate(2,0.0)
+
+    for (x in 0..1) {ReturnList[x] =WpnList[x] * 1.0}
+    return ReturnList
 }
--- app/src/test/java/com/example/biomescalculator/ExampleUnitTest.kt
+++ app/src/test/java/com/example/biomescalculator/ExampleUnitTest.kt
@@ -9,9 +9,68 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class ProbabilityStats {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun small_collection1() {
+        val WpnList = mutableListOf(0,1)
+        val WpnPow = mutableListOf(0,1)
+
+        var ProbList = getProbList(WpnList,WpnPow,1)
+        val TargetProb = listOf(0.0,1.0)
+        assertEquals(TargetProb, ProbList)
+    }
+    @Test
+    fun small_collection2() {
+        val WpnList = mutableListOf(1,0)
+        val WpnPow = mutableListOf(0,1)
+
+        var ProbList = getProbList(WpnList,WpnPow,1)
+        val TargetProb = listOf(1.0,0.0)
+        assertEquals(TargetProb, ProbList)
+    }
+    @Test
+    fun small_collection3() {
+        val WpnList = mutableListOf(0,1)
+        val WpnPow = mutableListOf(1,0)
+
+        var ProbList = getProbList(WpnList,WpnPow,1)
+        val TargetProb = listOf(0.0,1.0)
+        assertEquals(TargetProb, ProbList)
+    }
+    @Test
+    fun small_collection4a() {
+        val WpnList = mutableListOf(1,1)
+        val WpnPow = mutableListOf(1,0)
+
+        var ProbList = getProbList(WpnList,WpnPow,1)
+        val TargetProb = listOf(.5,.5)
+        assertEquals(TargetProb, ProbList)
+    }
+    @Test
+    fun small_collection4b() {
+        val WpnList = mutableListOf(2,1)
+        val WpnPow = mutableListOf(1,0)
+
+        var ProbList = getProbList(WpnList,WpnPow,1)
+        val TargetProb = listOf(1.0/3,2.0/3)
+        assertEquals(TargetProb, ProbList)
+    }
+    @Test
+    fun small_collection5() {
+        val WpnList = mutableListOf(1,1,1)
+        val WpnPow = mutableListOf(2,1,0)
+
+        var ProbList = getProbList(WpnList,WpnPow,1)
+        val TargetProb = listOf(1.0/3,1.0/3,1.0/3)
+        assertEquals(TargetProb, ProbList)
+    }
+    @Test
+    fun small_collection6() {
+        val WpnList = mutableListOf(1,1,1)
+        val WpnPow = mutableListOf(1,1,0)
+
+        var ProbList = getProbList(WpnList,WpnPow,1)
+        val TargetProb = listOf(1.0/3,2.0/3)
+        assertEquals(TargetProb, ProbList)
     }
 }
