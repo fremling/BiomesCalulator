d71c6dfdab65ac53a61e796f3f36bfc287791b40
Mikael Fremling <micke.fremling@gmail.com>
2022-01-14 13:46:05 +0100

    Added the buttons (now disables thoug due to debuggin

--- app/src/main/java/com/example/biomescalculator/MainActivity.kt
+++ app/src/main/java/com/example/biomescalculator/MainActivity.kt
@@ -3,41 +3,53 @@ package com.example.biomescalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.biomescalculator.databinding.ActivityMainBinding
+import android.util.Log
 
-//Varialbe for binding
+//Variable for binding
 lateinit var binding: ActivityMainBinding
 
-val NoWeaponsList = mutableListOf(0, 0, 0)
-val WeaponNameList = listOf("Potato", "Wood", "Stone")
-val WeaponStrengthList = listOf(0, 1, 2)
-val NoWpnButtons = 3
+private const val TAG = "MainActivity"
+
+val NoWpnButtons = 9
+val NoWeaponsList = generate(NoWpnButtons,0)
+val WeaponStrengthList = listOf(0, 1, 2,3,4,6,2,2,2)
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        Log.d(TAG, "Setting the inital variables")
+
         binding.BattleStats.text = "Welcome To the Battle Calculator\n\nPlease choose your weapons!"
 
         //Initiate the list of weapons
-
+        ResetWeapons()
         UpdateWpnText()
 
-        binding.weapon1.setOnClickListener { AddWeapon(1) }
-        binding.weapon2.setOnClickListener { AddWeapon(2) }
-        binding.weapon3.setOnClickListener { AddWeapon(3) }
+//        binding.weapon1.setOnClickListener { AddWeapon(1) }
+//        binding.weapon2.setOnClickListener { AddWeapon(2) }
+        //       binding.weapon3.setOnClickListener { AddWeapon(3) }
 
-        binding.Reset.setOnClickListener { ResetWeapons() }
+//        binding.Reset.setOnClickListener { ResetWeapons() }
 
-        binding.CalculateStats.setOnClickListener { CalulateStats() }
+//        binding.CalculateStats.setOnClickListener { calculateStats() }
     }
 
     private fun ResetWeapons() {
         for (x in 0..(NoWpnButtons - 1)) {
             NoWeaponsList[x] = 0
         }
+        //Set the standard weapons
+        NoWeaponsList[0] = 3
+        NoWeaponsList[1] = 1
+        NoWeaponsList[2] = 1
         binding.BattleStats.text = "Welcome To the Battle Calculator\n\nPlease choose your weapons!"
         UpdateWpnText()
     }
@@ -47,27 +59,32 @@ class MainActivity : AppCompatActivity() {
         UpdateWpnText()
     }
 
-    private fun CalulateStats() {
+    private fun calculateStats() {
         //Sum the total number of wepons
         val TotalNoWeapons = NoWeaponsList.sum()
         var TotalAttack = 0
+        var NumCards = 3
         for (x in 0..(NoWpnButtons - 1)) {
             TotalAttack += NoWeaponsList[x] * WeaponStrengthList[x]
         }
         val Str1 = "You have choosen ${TotalNoWeapons} weapons"
         val Str2 = "The total strength is ${TotalAttack}"
 
-        val ProbList = getProbList(NoWeaponsList,WeaponStrengthList,1)
+        if (TotalNoWeapons < NumCards ) {
+            binding.BattleStats.text = "Too few weapons. You need at least ${NumCards}"
+            return
+        }
+        val ProbList = getProbList(NoWeaponsList, WeaponStrengthList, NumCards)
         var Average = 0.0
         val Str3 = StringBuilder()
-        for (n in 0..(ProbList.size-1)) {
-            Str3.append("${n}: "+"%.1f".format(ProbList[n]*100)+"%\n")
-            Average+=ProbList[n]*n
+        for (n in 0..(ProbList.size - 1)) {
+            Str3.append("${n}: " + "%.1f".format(ProbList[n] * 100) + "%\n")
+            Average += ProbList[n] * n
         }
 
-    var Str4 = "Average attack is "+"%.1f".format(Average)
+        var Str4 = "Average attack is " + "%.1f".format(Average)
 
-        binding.BattleStats.text = Str1 + "\n" + Str2 + "\n" + Str3.toString()+ "\n"+Str4
+        binding.BattleStats.text = Str1 + "\n" + Str2 + "\n" + Str3.toString() + "\n" + Str4
     }
 
 
@@ -75,6 +92,13 @@ class MainActivity : AppCompatActivity() {
         binding.NoWpn1.text = NoWeaponsList[0].toString()
         binding.NoWpn2.text = NoWeaponsList[1].toString()
         binding.NoWpn3.text = NoWeaponsList[2].toString()
+        binding.NoWpn4.text = NoWeaponsList[3].toString()
+        binding.NoWpn5.text = NoWeaponsList[4].toString()
+        binding.NoWpn6.text = NoWeaponsList[5].toString()
+        binding.NoWpn7.text = NoWeaponsList[6].toString()
+        binding.NoWpn8.text = NoWeaponsList[7].toString()
+        binding.NoWpn9.text = NoWeaponsList[8].toString()
+
     }
 }
 
@@ -82,10 +106,21 @@ fun <T> generate(size: Int, value: T): MutableList<T> {
     return (0 until size).map { value }.toMutableList()
 }
 
-fun getProbList(WpnList: List<Int>, WpnPowList: List<Int>, NumCards: Int): List<Double> {
+fun getProbList(WpnList: List<Int>, WpnPowList: List<Int>, NumCards: Int): MutableList<Double> {
 
-    if (NumCards > 1) { throw java.lang.Exception("Only one card implemented yet") }
-    if (WpnList.size != WpnPowList.size) { throw java.lang.Exception("Lists of unequal length") }
+    if (NumCards < 0) {
+        throw java.lang.Exception("Cannot draw negative amount of cards")
+    }
+    if (NumCards == 0) {
+        return generate(1,1.0)
+    }
+
+    if (NumCards > WpnList.sum()) {
+        throw java.lang.Exception("Drawing more cards than weapons")
+    }
+    if (WpnList.size != WpnPowList.size) {
+        throw java.lang.Exception("Lists of unequal length")
+    }
 
     /*If we can only drw one cards, the maxium value is given by the most powerful weapon card */
     var LargestVal = 0
@@ -96,17 +131,48 @@ fun getProbList(WpnList: List<Int>, WpnPowList: List<Int>, NumCards: Int): List<
             }
         }
     }
-    // Craete the list of apropriate length
-    val ReturnList: MutableList<Double> = generate(LargestVal+1, 0.0)
+    // Create the list of aproriate length
+    var ReturnList: MutableList<Double> = generate(LargestVal + 1, 0.0)
     // Lookp through the weapons list and find the probability for each weapon
     val TotalNoWeapons = WpnList.sum()
     for (n in 0..(WpnList.size - 1)) {
-        val Prob = (1.0 *WpnList[n])/(TotalNoWeapons)
+        val Prob = (1.0 * WpnList[n]) / (TotalNoWeapons)
         if (Prob != 0.0) {
-            ReturnList[WpnPowList[n]] += Prob
+            val Power = WpnPowList[n]
+            if (NumCards == 1) {
+                ReturnList[Power] += Prob
+            } else {
+                val NewWpnList = WpnList.toMutableList()
+                NewWpnList[n] -= 1 //Reduce the new list by one
+                val NewProbList = getProbList(NewWpnList, WpnPowList, NumCards - 1)
+                ReturnList = mergeReturnList(ReturnList, NewProbList, Prob, Power)
+                //throw java.lang.Exception("${NewProbList}")
+            }
+
+
         }
     }
 
-
     return ReturnList
 }
+
+// Merges mutable lsits with a possible shift and scale
+fun mergeReturnList(
+    TargetList: MutableList<Double>,
+    ListAdd: MutableList<Double>,
+    scale: Double, IndxShift: Int
+): MutableList<Double> {
+    if (TargetList.size < ListAdd.size + IndxShift) {
+        //Extend the list so that they are of matching lengths
+        val ExtraNeed = ListAdd.size + IndxShift - TargetList.size
+        val ExtraVals=generate(ExtraNeed,0.0)
+        TargetList.addAll(ExtraVals)
+    }
+
+    //Large enough continue
+    for (n in 0..(ListAdd.size - 1)) {
+        TargetList[n + IndxShift] += ListAdd[n] * scale
+    }
+    return TargetList
+}
+
--- app/src/test/java/com/example/biomescalculator/ExampleUnitTest.kt
+++ app/src/test/java/com/example/biomescalculator/ExampleUnitTest.kt
@@ -1,8 +1,11 @@
 package com.example.biomescalculator
 
+import org.junit.Assert
 import org.junit.Test
 
 import org.junit.Assert.*
+import kotlin.math.abs
+import kotlin.math.pow
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -16,8 +19,8 @@ class ProbabilityStats {
         val WpnPow = mutableListOf(0,1)
 
         var ProbList = getProbList(WpnList,WpnPow,1)
-        val TargetProb = listOf(0.0,1.0)
-        assertEquals(TargetProb, ProbList)
+        val TargetProb = mutableListOf(0.0,1.0)
+        assertApproxEquals(TargetProb, ProbList)
     }
     @Test
     fun small_collection2() {
@@ -25,8 +28,8 @@ class ProbabilityStats {
         val WpnPow = mutableListOf(0,1)
 
         var ProbList = getProbList(WpnList,WpnPow,1)
-        val TargetProb = listOf(1.0)
-        assertEquals(TargetProb, ProbList)
+        val TargetProb = mutableListOf(1.0)
+        assertApproxEquals(TargetProb, ProbList)
     }
     @Test
     fun small_collection3() {
@@ -34,8 +37,8 @@ class ProbabilityStats {
         val WpnPow = mutableListOf(1,0)
 
         var ProbList = getProbList(WpnList,WpnPow,1)
-        val TargetProb = listOf(0.0,1.0)
-        assertEquals(TargetProb, ProbList)
+        val TargetProb = mutableListOf(0.0,1.0)
+        assertApproxEquals(TargetProb, ProbList)
     }
     @Test
     fun small_collection4a() {
@@ -43,8 +46,8 @@ class ProbabilityStats {
         val WpnPow = mutableListOf(1,0)
 
         var ProbList = getProbList(WpnList,WpnPow,1)
-        val TargetProb = listOf(.5,.5)
-        assertEquals(TargetProb, ProbList)
+        val TargetProb = mutableListOf(.5,.5)
+        assertApproxEquals(TargetProb, ProbList)
     }
     @Test
     fun small_collection4b() {
@@ -52,8 +55,8 @@ class ProbabilityStats {
         val WpnPow = mutableListOf(1,0)
 
         var ProbList = getProbList(WpnList,WpnPow,1)
-        val TargetProb = listOf(1.0/3,2.0/3)
-        assertEquals(TargetProb, ProbList)
+        val TargetProb = mutableListOf(1.0/3,2.0/3)
+        assertApproxEquals(TargetProb, ProbList)
     }
     @Test
     fun small_collection5() {
@@ -61,8 +64,8 @@ class ProbabilityStats {
         val WpnPow = mutableListOf(2,1,0)
 
         var ProbList = getProbList(WpnList,WpnPow,1)
-        val TargetProb = listOf(1.0/3,1.0/3,1.0/3)
-        assertEquals(TargetProb, ProbList)
+        val TargetProb = mutableListOf(1.0/3,1.0/3,1.0/3)
+        assertApproxEquals(TargetProb, ProbList)
     }
     @Test
     fun small_collection6() {
@@ -70,7 +73,175 @@ class ProbabilityStats {
         val WpnPow = mutableListOf(1,1,0)
 
         var ProbList = getProbList(WpnList,WpnPow,1)
-        val TargetProb = listOf(1.0/3,2.0/3)
-        assertEquals(TargetProb, ProbList)
+        val TargetProb = mutableListOf(1.0/3,2.0/3)
+        assertApproxEquals(TargetProb, ProbList)
+    }
+
+    @Test
+    fun draw0_a() {
+        val WpnList = mutableListOf(1,1,0)
+        val WpnPow = mutableListOf(0,1,2)
+
+        var ProbList = getProbList(WpnList,WpnPow,0)
+        val TargetProb = mutableListOf(1.0)
+        assertApproxEquals(TargetProb, ProbList)
+    }
+
+    @Test
+    fun draw2_a() {
+        val WpnList = mutableListOf(1,1,0)
+        val WpnPow = mutableListOf(0,1,2)
+
+        var ProbList = getProbList(WpnList,WpnPow,2)
+        val TargetProb = mutableListOf(0.0,1.0)
+        assertApproxEquals(TargetProb, ProbList)
+    }
+    @Test
+    fun draw2_b() {
+        val WpnList = mutableListOf(1,1,1)
+        val WpnPow = mutableListOf(0,1,2)
+
+        var ProbList = getProbList(WpnList,WpnPow,2)
+        val TargetProb = mutableListOf(0.0,1.0/3,1.0/3,1.0/3)
+        assertApproxEquals(TargetProb, ProbList)
+    }
+    @Test
+    fun draw2_c() {
+        val WpnList = mutableListOf(2,0,2)
+        val WpnPow = mutableListOf(0,1,2)
+
+        var ProbList = getProbList(WpnList,WpnPow,2)
+        val TargetProb = mutableListOf(1/6.0,0.0,4.0/6,0.0,1.0/6)
+        assertApproxEquals(TargetProb, ProbList)
+    }
+    @Test
+    fun draw3_0() {
+        val WpnList = mutableListOf(1,1,1)
+        val WpnPow = mutableListOf(0,1,2)
+
+        var ProbList = getProbList(WpnList,WpnPow,3)
+        val TargetProb = mutableListOf(0.0,0.0,0.0,1.0)
+        assertApproxEquals(TargetProb, ProbList)
+    }
+    @Test
+    fun draw3_1() {
+        val WpnList = mutableListOf(1,0,2)
+        val WpnPow = mutableListOf(0,1,2)
+
+        var ProbList = getProbList(WpnList,WpnPow,3)
+        val TargetProb = mutableListOf(0.0,0.0,0.0,0.0,1.0)
+        assertApproxEquals(TargetProb, ProbList)
+    }
+    @Test
+    fun draw3_2() {
+        val WpnList = mutableListOf(0,3,0)
+        val WpnPow = mutableListOf(0,1,2)
+
+        var ProbList = getProbList(WpnList,WpnPow,3)
+        val TargetProb = mutableListOf(0.0,0.0,0.0,1.0)
+        assertApproxEquals(TargetProb, ProbList)
+    }
+
+    @Test
+    fun draw3_a() {
+        val WpnList = mutableListOf(2,0,2)
+        val WpnPow = mutableListOf(0,1,2)
+
+        var ProbList = getProbList(WpnList,WpnPow,3)
+        val TargetProb = mutableListOf(0.0,0.0,0.5,0.0,1.0/2)
+        assertApproxEquals(TargetProb, ProbList)
+    }
+
+    @Test
+    fun draw3_b() {
+        val WpnList = mutableListOf(1,1,2)
+        val WpnPow = mutableListOf(0,1,2)
+
+        var ProbList = getProbList(WpnList,WpnPow,3)
+        val TargetProb = mutableListOf(0.0,0.0,0.0,1/2.0,1.0/4,1.0/4)
+        assertApproxEquals(TargetProb, ProbList)
+    }
+
+}
+
+class MergeLists {
+    @Test
+    fun test_list_merge_0() {
+        val ListA = mutableListOf(.2,1.0)
+        val ListB = mutableListOf(.0,0.0)
+        val MergeList = mergeReturnList(ListA,ListB,1.0,0)
+        val ResList = mutableListOf(.2,1.0)
+        assertApproxEquals(ResList,MergeList)
+    }
+
+    @Test
+    fun test_list_merge_1() {
+        val ListA = mutableListOf(.2,0.0,1.0,.3)
+        val ListB = mutableListOf(.1,0.0,1.0,.3)
+        val MergeList = mergeReturnList(ListA,ListB,1.0,0)
+        val ResList = mutableListOf(.3,0.0,2.0,.6)
+        assertApproxEquals(ResList,MergeList)
+    }
+
+    @Test
+    fun test_list_merge_2() {
+        val ListA = mutableListOf(.2,0.0,1.0,.3)
+        val ListB = mutableListOf(.1,0.0)
+        val MergeList = mergeReturnList(ListA,ListB,1.0,0)
+        val ResList = mutableListOf(.3,0.0,1.0,.3)
+        assertApproxEquals(ResList,MergeList)
+    }
+    @Test
+    fun test_list_merge_3() {
+        val ListA = mutableListOf(.2,0.0,1.0,.3)
+        val ListB = mutableListOf(.1,0.0)
+        val MergeList = mergeReturnList(ListA,ListB,2.0,0)
+        val ResList = mutableListOf(.4,0.0,1.0,.3)
+        assertApproxEquals(ResList,MergeList)
+    }
+    @Test
+    fun test_list_merge_4() {
+        val ListA = mutableListOf(.2,0.0,1.0,.3)
+        val ListB = mutableListOf(.1,2.0)
+        val MergeList = mergeReturnList(ListA,ListB,2.0,2)
+        val ResList = mutableListOf(.2,0.0,1.2,4.3)
+        assertApproxEquals(ResList,MergeList)
+    }
+    @Test
+    fun test_list_merge_5() {
+        val ListA = mutableListOf(.2)
+        val ListB = mutableListOf(.1,2.0)
+        val MergeList = mergeReturnList(ListA,ListB,2.0,1)
+        val ResList = mutableListOf(.2,0.2,4.0)
+        assertApproxEquals(ResList,MergeList)
+    }
+    @Test
+    fun test_list_merge_6() {
+        val ListA = mutableListOf<Double>()
+        val ListB = mutableListOf(.1,2.0)
+        val MergeList = mergeReturnList(ListA,ListB,2.0,1)
+        val ResList = mutableListOf(0.0,0.2,4.0)
+        assertApproxEquals(ResList,MergeList)
+    }
+    @Test
+    fun test_list_merge_7() {
+        val ListA = mutableListOf(.2)
+        val ListB = mutableListOf(.1,0.0)
+        val MergeList = mergeReturnList(ListA,ListB,1.0,0)
+        val ResList = mutableListOf(.3,0.0)
+        assertApproxEquals(ResList,MergeList)
+    }
+}
+
+fun assertApproxEquals(ListA : MutableList<Double>, ListB : MutableList<Double>) {
+    val atol = (10.0).pow(-5)
+    assertEquals(ListA.size,ListB.size)
+    Assert.assertEquals(ListA.sum(),1.0,atol)
+    Assert.assertEquals(ListB.sum(),1.0,atol)
+
+    for (n in 0..(ListA.size-1)) {
+        if (abs(ListA[n] - ListB[n]) > atol) {
+            assertEquals(ListA, ListB)
+        }
     }
 }
